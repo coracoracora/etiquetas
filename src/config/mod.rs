caa7dbac649fa0bfd220175c72064a2b4dc98c55
Cora Middleton <coravore@gmail.com>
2025-05-15 16:09:03 +0100

    Add configuration module for managing app paths ```
    Includes utilities for accessing and creating user config, cache,
    and app data directories. Implements a `ConfigPath` trait for
    common path operations and provides specific structs for each
    path type.
    ```

---
+++ src/config/mod.rs
@@ -0,0 +1,43 @@
+mod paths;
+use anyhow::Result;
+use paths::{AppData, CacheConfig, UserConfig};
+use std::path::{Path, PathBuf};
+
+/// Get or create a subdirectory under user config.
+/// - [`subpath`]: If Some(P), a subdirectory will be created if necessary
+///   and returned. If None, the root config directory will be returned.
+pub fn get_or_create_user_config_path<P: AsRef<Path>>(subpath: Option<P>) -> Result<PathBuf> {
+    UserConfig::get_config_path(subpath)
+}
+
+/// Get or create a subdirectory in the app cache.
+/// - [`subpath`]: If Some(P), a subdirectory will be created if necessary
+///   and returned. If None, the root cache directory will be returned.
+pub fn get_or_create_cache_path<P: AsRef<Path>>(subpath: Option<P>) -> Result<PathBuf> {
+    CacheConfig::get_cache_path(subpath)
+}
+
+/// Get or create a subdirectory in the app data dir.
+/// - [`subpath`]: If Some(P), a subdirectory will be created if necessary
+///   and returned. If None, the root app data directory will be returned.
+pub fn get_or_create_app_data_path<P: AsRef<Path>>(subpath: Option<P>) -> Result<PathBuf> {
+    AppData::get_data_path(subpath)
+}
+
+/// TBI
+pub fn get_shadow_cache_path() -> Result<PathBuf> {
+    get_or_create_cache_path("shadow-index".into())
+}
+
+/// Get or create a path inside the embeddings cache.
+pub fn get_embeddings_cache_path<P: AsRef<Path>>(subpath: Option<P>) -> Result<PathBuf> {
+    let embeddings_subpath = PathBuf::from("embeddings");
+    get_or_create_cache_path(Some(
+        subpath
+            .map(|s| embeddings_subpath.join(s))
+            .unwrap_or(embeddings_subpath),
+    ))
+}
+
+#[cfg(test)]
+mod tests {}
